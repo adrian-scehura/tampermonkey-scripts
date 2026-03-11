16e473b991cbc87442ec6bbc1e4361a31640e140
Adrian Scehura <adrian.scehura@gmail.com>
2026-03-11 09:22:50 +0100

    update URLs to point to the correct raw GitHub paths

--- github-pr-links.js
+++ github-pr-links.js
@@ -4,8 +4,8 @@
 // @version      1.3.1
 // @description  Adds a button to copy the PR title as a rich text link
 // @match        https://github.com/*/*/pull/*
-// @updateURL    https://raw.githubusercontent.com/adrian-scehura/tampermonkey-scripts/main/github-pr-links.js
-// @downloadURL  https://raw.githubusercontent.com/adrian-scehura/tampermonkey-scripts/main/github-pr-links.js
+// @updateURL    https://raw.githubusercontent.com/adrian-scehura/tampermonkey-scripts/refs/heads/main/github-pr-links.js
+// @downloadURL  https://raw.githubusercontent.com/adrian-scehura/tampermonkey-scripts/refs/heads/main/github-pr-links.js
 // @grant        GM_setClipboard
 // ==/UserScript==
 
